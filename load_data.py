38d81dc8baa8cec35b7dd9a0dcc571e5fdc12be4
jana mawasy <janamawasy99@gmail.com>
2024-05-27 02:06:14 +0300

    database new structure to fit many to many - should check it out

--- load_data.py
+++ load_data.py
@@ -1,8 +1,6 @@
 import json
-
 from sqlalchemy.orm import sessionmaker
-
-from models import *
+from models import Pokemon, Trainer, Trainer_Enrollment, Type, Type_Enrollment, engine
 
 # Create a Session
 Session = sessionmaker(bind=engine)
@@ -16,20 +14,41 @@ def loadDB():
 
     # Insert data into database
     for entry in data:
+        # Insert Pokemon
         pokemon = Pokemon(id=entry['id'], name=entry['name'], height=entry['height'], weight=entry['weight'])
         session.add(pokemon)
 
-        # Insert trainers
-        for trainer in entry.get('ownedBy', []):
-            new_trainer = Trainer(name=trainer['name'], town=trainer['town'], pokemon=pokemon)
-            session.add(new_trainer)
+        # Insert Trainers and Trainer_Enrollments
+        for trainer_data in entry.get('ownedBy', []):
+            # Check if the trainer already exists
+            trainer = session.query(Trainer).filter_by(name=trainer_data['name'], town=trainer_data['town']).first()
+            if not trainer:
+                trainer = Trainer(name=trainer_data['name'], town=trainer_data['town'])
+                session.add(trainer)
+                session.flush()  # Ensures trainer.id is available
 
-        # Insert types
+            trainer_enrollment = Trainer_Enrollment(trainer_id=trainer.id, poke_id=pokemon.id)
+            session.add(trainer_enrollment)
+
+        # Insert Types and Type_Enrollments
         for type_name in entry.get('types', []):
-            new_type = Type(type=type_name, pokemon=pokemon)
-            session.add(new_type)
+            # Check if the type already exists
+            type_instance = session.query(Type).filter_by(type=type_name).first()
+            if not type_instance:
+                type_instance = Type(type=type_name)
+                session.add(type_instance)
+                session.flush()  # Ensures type_instance.id is available
 
-session.commit()
-session.close()
+            type_enrollment = Type_Enrollment(type_id=type_instance.id, poke_id=pokemon.id)
+            session.add(type_enrollment)
 
-print("Database has been populated with SQLAlchemy.")
+    # Commit the transaction
+    session.commit()
+    # Close the session
+    session.close()
+
+    print("Database has been populated with SQLAlchemy.")
+
+
+# Call loadDB to populate the database with data from JSON file
+loadDB()
--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 
+
 Base = declarative_base()
 
 
@@ -11,27 +12,39 @@ class Pokemon(Base):
     name = Column(String(255))
     height = Column(Integer)
     weight = Column(Integer)
-    trainers = relationship('Trainer', back_populates='pokemon')
+    trainers = relationship('Trainer_Enrollment', back_populates='pokemon')
     types = relationship('Type', back_populates='pokemon')
 
 
 class Trainer(Base):
-    __tablename__ = 'trainer'
+    __tablename__ = 'trainers'
     id = Column(Integer, primary_key=True)
     name = Column(String(255))
     town = Column(String(255))
-    poke_id = Column(Integer, ForeignKey('pokemon.id'))
-    pokemon = relationship('Pokemon', back_populates='trainers')
 
+
+class Trainer_Enrollment(Base):
+    __tablename__ = 'trainer_enrollment'
+    id = Column(Integer, primary_key=True)
+    trainer_id = Column(Integer)
+    poke_id = Column(Integer, ForeignKey('pokemon.id'))
+    trainer = relationship(Trainer, back_populates='pokemons')
+    pokemon = relationship(Pokemon, back_populates='trainers')
 
 class Type(Base):
     __tablename__ = 'types'
     id = Column(Integer, primary_key=True)
     type = Column(String(255))
+
+class Type_Enrollment(Base):
+    __tablename__ = 'types_enrollment'
+    id = Column(Integer, primary_key=True)
+    type_id = Column(Integer)
     poke_id = Column(Integer, ForeignKey('pokemon.id'))
     pokemon = relationship('Pokemon', back_populates='types')
+    type = relationship('Type', back_populates='types')
 
 
 # Database connection setup
-engine = create_engine('mysql+pymysql://root:@localhost/pokemonsdb')
+engine = create_engine('mysql+pymysql://root:@localhost/pokemons_db')
 Base.metadata.create_all(engine)  # Creates the tables
